7fbdf362e6f834a2851fc52499acdf40c8aaa79c
Carlos Gines <cgi@phatconsulting.de>
2017-04-27 18:06:30 +0200

    Added documentation for CheckCondition and ResolveName projects.

---
+++ Manual/Extensions/Tools/_static/CheckConditionSampleScript.ps1
@@ -0,0 +1,19 @@
+﻿Add-Type -Path ("$PSScriptRoot\bin\debug\CheckCondition.dll")
+
+#Here are 2 examples of use of the class SIM.Tools.Condition, with different inputs.
+
+#Given some inputs (be sure to use simple qutations for string literals: 'text'):
+$numInput = "3 * (2 + 1) = 9"
+$stringInput = "'M' ! 'R'"
+
+#We create the Condition:
+$numCondition = New-Object SIM.Tools.Condition($numInput)
+$stringCondition = New-Object SIM.Tools.Condition($stringInput)
+
+#And so we can access the condition result:
+$numCondition.get_Result()
+$stringCondition.get_Result()
+
+#We could also check the original condition from input:
+#$numCondition.Condition
+#$stringCondition.Condition
