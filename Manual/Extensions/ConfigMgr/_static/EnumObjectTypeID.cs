7f3670de486f9a1ffa72adc24ed2a47186512362
silvermonkey2 <20575757+silvermonkey2@users.noreply.github.com>
2018-09-28 14:32:41 +0200

    Add files via upload

---
+++ Manual/Extensions/ConfigMgr/_static/EnumObjectTypeID.cs
@@ -0,0 +1,10 @@
+    public enum ObjectTypeID
+    {
+        SMS_Package = 2,
+        SMS_OperatingSystemInstallPackage = 14,
+        SMS_ImagePackage = 18,
+        SMS_BootImagePackage = 19,
+        SMS_DriverPackage = 23,
+        SMS_SoftwareUpdatesPackage = 24,
+        SMS_Application = 31
+    }
