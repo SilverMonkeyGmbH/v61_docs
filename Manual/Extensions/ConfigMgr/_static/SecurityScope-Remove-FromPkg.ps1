8a48d6af276bf7e061ab3818597bef242eb5d36a
silvermonkey2 <20575757+silvermonkey2@users.noreply.github.com>
2018-09-28 14:27:55 +0200

    Add files via upload

---
+++ Manual/Extensions/ConfigMgr/_static/SecurityScope-Remove-FromPkg.ps1
@@ -0,0 +1,27 @@
+$Assembly = [Reflection.Assembly]::LoadFile("$PSScriptRoot\Base.dll")
+$Assembly = [Reflection.Assembly]::LoadFile("$PSScriptRoot\Database.dll")
+$Assembly = [Reflection.Assembly]::LoadFile("$PSScriptRoot\ConfigMgr.dll")
+
+$ConfigMgrConnectionSettings = New-Object SIM.ConfigMgr.ConnectionSettings
+$ConfigMgrConnectionSettings.WMIHostName = 'localhost'
+$ConfigMgrConnectionSettings.SiteCode = 'P01'
+$ConfigMgrConnectionSettings.SQLHostname = $ConfigMgrConnectionSettings.WMIHostName;
+
+$ConfigMgrConnection = New-Object SIM.ConfigMgr.Connection($ConfigMgrConnectionSettings)
+
+$res = New-Object Base.Result("Starting ConfigMgr script...")
+
+$res.ChildAdd($ConfigMgrConnection.Connect())
+
+if ($res.Successful -eq $true)
+{
+
+    $SecurityScopeManagement = New-Object SIM.ConfigMgr.Security.SecurityScopeManagement($ConfigMgrConnection);
+
+    $securityScope = New-Object SIM.ConfigMgr.Security.SecurityScope ("","GERMANY Scope","") 
+
+    $res.ChildAdd( ($SecurityScopeManagement.SecurityScopeRemove($securityScope, "P0100052", [SIM.ConfigMgr.ObjectTypeID]::SMS_Package) ));
+   
+}
+
+$res.Dump()
